576e47ceebc5bee0ebb0553a829fea9f42e6812d
Lucas Araujo Alves <lucasaraujoalves0312@gmail.com>
2026-08-13 22:18:20 -0300

    Nivel inicial do desafio

--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+public class Main{
+    public static void main(String[] args){
+        Personagem player1 = new Personagem();
+        player1.nome = "Art";
+        player1.classe = "Mago";
+        player1.nivel = 99;
+        player1.vida = 265;
+        player1.poder = 1350;
 
-        .
-        .
-        .
+        player1.exibirStatus();
+        System.out.println();
 
+        Personagem player2 = new Personagem();
+        player2.nome = "Priex";
+        player2.classe = "Assassino";
+        player2.nivel = 99;
+        player2.vida = 445;
+        player2.poder = 995;
+
+        player2.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,26 @@
 public class Personagem {
-    String nome;
-    String classe;
-    int nivel;
-    int pontosDeVida;
-    double poderBase;
+    public String nome;
+    public String classe;
+    public int nivel;
+    public int vida;
+    public double poder;
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+    public void Personagem(String nome, String classe, int nivel, int vida, double poder){
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.vida = vida;
+        this.poder = poder;
     }
 
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+    public void exibirStatus(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Vida: " + vida);
+        System.out.println("Poder: " + poder);
 
     }
 }
+
 
